94ff4566987af71e88c1edaaf0d3ef838a1039f4
Esteban <Esteban.sua.95@gmail.com>
2024-03-19 13:12:19 -0500

    se agrega rastreador y conteo de carros

--- yolo_deteccion_carros.py
+++ yolo_deteccion_carros.py
@@ -27,6 +27,8 @@ mask = cv2.imread('imagenes/mascara720.png') #cargamos la mascara para delimitar
 
 #llamamos el rastreador 
 rastreador = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
+limite = [300,370,725,370]
+conteo = []
 
 while True:
     
@@ -34,6 +36,8 @@ while True:
     zona= cv2.bitwise_and(frame,mask) #se crea la zona de interes
   
     result = modelo(zona,stream = True)
+
+    cv2.line(frame,(limite[0], limite[1]), (limite[2], limite[3]),(0, 0, 255),4)
 
     #lista de detecciones
     detecciones = np.empty((0, 5)) 
@@ -53,20 +57,36 @@ while True:
             cls = int(box.cls[0])
 
             if classNames[cls] == "car" and conf > 0.3:
-                    cvzone.cornerRect(frame,(x1,y1,w,h),l =9, t =3) #dibujar rectangulo con cvzone
-                    cvzone.putTextRect(frame, f'{classNames[cls]} {conf}',(max(0,x1), max(35,y1)),scale= 0.5, thickness=1, offset = 3) #imprimimos en pantalla el valor de confianza
+                    #cvzone.cornerRect(frame,(x1,y1,w,h),l =9, t =3) #dibujar rectangulo con cvzone
+                    #cvzone.putTextRect(frame, f'{classNames[cls]} {conf}',(max(0,x1), max(35,y1)),scale= 0.5, thickness=1, offset = 3) #imprimimos en pantalla el valor de confianza
                     currentArray =np.array([x1, y1, x2, y2, conf]) # matriz actual de deteccion
                     detecciones = np.vstack((detecciones, currentArray)) # añadimos deteccion a la lista de detecciones
-            # cvzone.putTextRect(frame, f'{cls} {conf}',(max(0,x1), max(35,y1)),scale= 0.7, thickness=1)
-            #print(conf)
+            
 
     resultados_rastreador = rastreador.update(detecciones)
 
     #Recorremos la lista de resultado rastreador para obtener los ids
     for resultado in resultados_rastreador:
+         
          x1, y1, x2, y2, Id = resultado
-         print(resultado)
+         x1,y1,x2,y2 =  int(x1), int(y1), int(x2), int(y2) #convertimos a entero
+         w,h = x2-x1, y2-y1
+
+         cvzone.cornerRect(frame,(x1,y1,w,h),l =9, t =3, colorR=(255,0,0)) 
+         cvzone.putTextRect(frame, f'{int(Id)}',(max(0,x1), max(35,y1)),scale= 1, thickness=1, offset = 5) #imprimos ID 
+         #print(resultado)
+
+         cx, cy = x1+w//2, y1+h//2
+         cv2.circle(frame,(cx,cy),4 ,(255,0,255),cv2.FILLED)
+
+         if limite[0] < cx < limite [2] and limite[1]-20 < cy < limite [1]+ 20: 
+                         
+              if conteo.count(Id) == 0:
+                conteo.append(Id)             
+    
+    cvzone.putTextRect(frame, f'Contador{len(conteo)}',(50,50))      
+               
 
     cv2.imshow("video",frame)
     #cv2.imshow("region",zona)
-    cv2.waitKey(0)
+    cv2.waitKey(1)
